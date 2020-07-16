ecf5b5ea02fb1535368376ed87268f047ff9dc3e
Dean Moore <deanmoore009@gmail.com>
2020-07-16 20:16:40 +0100

    moving items added to the list

--- Website Test/js/script.js
+++ Website Test/js/script.js
@@ -1,50 +1,57 @@
-toadd = document.getElementById("gyminput").value;
+ // used to add an activity to the gym routine
 
-// used to add an activity to the gym routine
+ const toadd = document.getElementById("gyminput").value;
 
-function newListItem() {
-    let li = document.createElement("li");
-    var inputValue = document.getElementById("gyminput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("Please add an activity");
-    } else {
-        document.getElementById("activityList").appendChild(li);
-    }
+ function newListItem() {
+     let li = document.createElement("li");
+     var inputValue = document.getElementById("gyminput").value;
+     var t = document.createTextNode(inputValue);
+     li.appendChild(t);
+     if (inputValue === '') {
+         alert("Please add an activity");
+     } else {
+         document.getElementById("activityList").appendChild(li);
+     }
 
-    document.getElementById("gyminput").value = "";
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+     document.getElementById("gyminput").value = "";
+     var span = document.createElement("SPAN");
+     var txt = document.createTextNode("\u00D7");
+     span.className = "close";
+     span.appendChild(txt);
+     li.appendChild(span);
 
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-}
+     for (i = 0; i < close.length; i++) {
+         close[i].onclick = function () {
+             var div = this.parentElement;
+             div.style.display = "none";
+         }
+     }
 
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
+     // if an item is in the list, allow it to be dragged into the containment 'rightcontent'
+     $("li").draggable({
+         containment: ".rightcontent"
+     });
 
-// end of gym routing adding
+ }
+
+ var myNodelist = document.getElementsByTagName("li");
+ var i;
+ for (i = 0; i < myNodelist.length; i++) {
+     var span = document.createElement("SPAN");
+     var txt = document.createTextNode("\u00D7");
+     span.className = "close";
+     span.appendChild(txt);
+     myNodelist[i].appendChild(span);
+ }
+
+ var close = document.getElementsByClassName("close");
+ var i;
+ for (i = 0; i < close.length; i++) {
+     close[i].onclick = function () {
+         var div = this.parentElement;
+         div.style.display = "none";
+     }
+ }
+
+ // end of gym routine adding
