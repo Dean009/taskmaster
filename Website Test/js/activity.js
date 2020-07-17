44330e27714d94422f572594b9bd3478d201b799
Dean Moore <deanmoore009@gmail.com>
2020-07-17 19:36:53 +0100

    fixed dragging. added nav, seperated js, new files

--- Website Test/js/activity.js
+++ Website Test/js/activity.js
@@ -13,6 +13,12 @@
          document.getElementById("activityList").appendChild(li);
      }
 
+     var span = document.createElement("SPAN");
+     var txt = document.createTextNode("\u2261");
+     span.className = "add";
+     span.appendChild(txt);
+     li.appendChild(span);
+
      document.getElementById("gyminput").value = "";
      var span = document.createElement("SPAN");
      var txt = document.createTextNode("\u00D7");
@@ -53,5 +59,16 @@
          div.style.display = "none";
      }
  }
-
  // end of gym routine adding
+
+
+
+ // cookie banner - conflict with the id close with code above
+ if (localStorage.getItem('cookieSeen') != 'shown') {
+     $('.cookie-banner').delay(2000).fadeIn();
+     localStorage.setItem('cookieSeen', 'shown')
+ };
+
+ $('.close').click(function () {
+     $('.cookie-banner').fadeOut();
+ })
---
+++ Website Test/js/scroll.js
@@ -0,0 +1,4 @@
+//add js that makes the navbar change when you scroll down the page.
+//alternatively make the navbar only be able to be scrolled to the top of 
+//rightcoloum.
+// https://css-tricks.com/styling-based-on-scroll-position/
